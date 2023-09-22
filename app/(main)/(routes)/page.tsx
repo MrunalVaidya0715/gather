df84cef60d5e1b8d33b4519a045fd88b9e63280d
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-09-22 20:01:19 +0530

    theme-mode toggle

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -1,10 +1,18 @@
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center ">
+      <div className="z-[100] fixed flex items-center gap-2 top-0 left-0 p-2">
+        <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
+      </div>
       <div className="flex flex-col items-center">
-        <h1 className=" tracking-wider uppercase text-3xl font-bold text-blue-600">Gather</h1>
+        <h1 className=" tracking-wider uppercase text-3xl font-bold text-blue-600">
+          Gather
+        </h1>
         <p>The future of online socializing, made simple!</p>
       </div>
     </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Inter, Geologica } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 const font = Geologica({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,8 +18,17 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>{children}</body>
+      <html lang="en"  suppressHydrationWarning>
+        <body className={cn(font.className, "bg-white dark:bg-[#1d2332]")}>
+          <ThemeProvider 
+            attribute="class"
+            defaultTheme="dark"
+            enableSystem={false}
+            storageKey="gather-theme"
+            >
+          {children}
+          </ThemeProvider>
+        </body>
       </html>
     </ClerkProvider>
   );
