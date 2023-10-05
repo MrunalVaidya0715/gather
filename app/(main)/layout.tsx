1cb10851730532df49950c4f074a8db5743df726
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-10-05 22:38:09 +0530

    rm UserBtn nd ModeTgle

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,14 +1,10 @@
-import { ModeToggle } from "@/components/mode-toggle";
+
 import NavigationSidebar from "@/components/navigations/NavigationSidebar";
-import { UserButton } from "@clerk/nextjs";
 
 const MainLayout = async ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full">
-      <div className=" absolute z-[100] flex items-center gap-2 top-0 right-0 p-2">
-        <UserButton afterSignOutUrl="/" />
-        <ModeToggle />
-      </div>
+      
       <div className="fixed z-30 inset-y-0 hidden md:flex flex-col h-full w-[75px]">
         <NavigationSidebar />
       </div>
