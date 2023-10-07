0f7f4ab002de79adc77b737df90d1ea94bdc39df
MrunalVaidya0715 <110215192+MrunalVaidya0715@users.noreply.github.com>
2023-10-07 13:10:46 +0530

    feat: Server-Sidebar

---
+++ components/server/ServerHeader.tsx
@@ -0,0 +1,95 @@
+"use client"
+
+import { ServerWithMembersWithProfiles } from "@/types"
+import { MemberRole } from "@prisma/client"
+import { DropdownMenu,DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuLabel,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger, } from "@/components/ui/dropdown-menu";
+import { Button } from "../ui/button";
+import { ChevronDown, LogOut, PlusCircle, Settings, Trash, UserPlus, Users } from "lucide-react";
+
+interface ServerHProps{
+    server: ServerWithMembersWithProfiles,
+    role?: MemberRole
+}
+
+const ServerHeader = ({server, role}:ServerHProps) => {
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR
+  return (
+    <DropdownMenu>
+        <DropdownMenuTrigger className=" focus:outline-none" asChild>
+            <button className="w-full text-sm font-semibold p-2 flex items-center h-12 border-neutral-200 dark:border-neutral-600 border-[1px] hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition">
+                {server.name}
+                <ChevronDown className="h-5 w-5 ml-auto"/>
+            </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent
+        className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]"
+      >
+        {isModerator && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer"
+          >
+            Invite People
+            <UserPlus className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Server Settings
+            <Settings className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Manage Members
+            <Users className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="px-3 py-2 text-sm cursor-pointer"
+          >
+            Create Channel
+            <PlusCircle className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {isModerator && (
+          <DropdownMenuSeparator />
+        )}
+        {isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Delete Server
+            <Trash className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+        {!isAdmin && (
+          <DropdownMenuItem
+            onClick={() => {}}
+            className="text-rose-500 px-3 py-2 text-sm cursor-pointer"
+          >
+            Leave Server
+            <LogOut className="h-4 w-4 ml-auto" />
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+
+    </DropdownMenu>
+  )
+}
+
+export default ServerHeader
